f35c7d77db4e19aeccba641e88ed9be4deb65769
Eline <elineste@stud.ntnu.no>
2017-12-28 16:11:24 +0100

    Minor changes

--- project_carrot_cake/modules/wrangling.py
+++ project_carrot_cake/modules/wrangling.py
@@ -8,9 +8,9 @@ class wrangling(object):
     def msg_filter(self, msgs, list_of_tickers):
         '''Returns: df with messages from given tickers given a set of messages'''
         for ticker in list_of_tickers:
-            print("Ticker: " + ticker)
-            print("Indexes: " + msgs.index[msgs['ISSUER_ID'] == ticker].tolist())
+            print('Ticker: ' + ticker)
+            print('Indices: ' + msgs.index[msgs['ISSUER_ID'] == ticker].tolist())
             #print("DF: " + )
 
         #return filtered_messages
-        return 
+        return
